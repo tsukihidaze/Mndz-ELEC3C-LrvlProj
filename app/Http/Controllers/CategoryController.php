dc89849fde307de08529f22b3b6820c7dd67d36f
David <68171064+toratoratora-code@users.noreply.github.com>
2023-11-14 18:35:25 +0800

    category CRUD, display login name

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
@@ -14,13 +16,37 @@ class CategoryController extends Controller
         return view('admin.category.category', compact('categories'));
     }
 
-    public function create(Request $request)
+    public function Create(Request $request)
     {
         $category = new Category;
         $category->category_name = $request->input('category_name');
-        $category->user_id = $request->input('user_id');
+        $category->user_id = Auth::user()->id;
+        $category->created_at = Carbon::now();
 
         $category->save();
+
+        return Redirect::back();
+    }
+
+    public function Edit($id)
+    {
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
+    }
+
+    public function Update(Request $request, $id)
+    {
+        Category::find($id)->update([
+            'category_name' => $request->category_name,
+            'user_id' => Auth::user()->id,
+        ]);
+
+        return Redirect()->route('AllCat');
+    }
+
+    public function Delete($id)
+    {
+        Category::destroy($id);
 
         return Redirect::back();
     }
--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -10,11 +10,6 @@
             {{-- <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-welcome />
             </div> --}}
-
-            @php
-                $i = 1;
-            @endphp
-
             <div class="row">
                 <div class="col-md-8">
                     <table class="table">
@@ -22,30 +17,45 @@
                             <tr>
                                 <th scope="col">Id</th>
                                 <th scope="col">Category name</th>
-                                <th scope="col">User Id</th>
+                                <th scope="col">User name</th>
                                 <th scope="col">Created At</th>
+                                <th scope="col">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($categories as $category)
                                 <tr>
-                                    <td>@php $i++ @endphp</td>
+                                    <td>{{ $category->id }}</td>
                                     <td>{{ $category->category_name }}</td>
-                                    <td>{{ $category->user_id }}</td>
-                                    <td>{{ $category->created_at }}</td>
+                                    <td>{{ $category->user->name }}</td>
+                                    <td>{{ $category->created_at->diffForHumans() }}</td>
+                                    <td>
+                                        <a class="btn btn-primary"
+                                            href={{ route('category.edit', ['id' => $category->id]) }}>
+                                            Edit
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <form action={{ route('category.delete', ['id' => $category->id]) }}
+                                            method="POST">
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
                 <div class="col-md-4">
-                    <form action="{{ url('all/category/new') }}" method="POST">
+                    <form action={{ route('category.add') }} method="POST">
                         @csrf
                         <div class="form-group">
                             <label for="category_name">Category Name</label>
                             <input type="text" class="form-control" id="category_name" aria-describedby="emailHelp"
                                 name="category_name" placeholder="Category Name">
-                            <input type="hidden" name="user_id" value="1" />
                         </div>
                         <button type="submit" class="btn btn-success">Submit</button>
                     </form>
